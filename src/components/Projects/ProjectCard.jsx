b8ea6e20ad7631c5994a92a1ae2072b8e202ea30
anuragsingh013 <thakuranuragsingh099@gmail.com>
2024-05-15 17:53:20 +0530

    Added React parallax tilt functionality

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-
+import Tilt from 'react-parallax-tilt';
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({ project: { title, imageSrc, description, skills, demo, source } }) => {
 
   return (
-    <div className={styles.container}>
-      <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image} />
-     <div className="info-section">
-     <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
-      {/* <ul className={styles.skills}>
+    <Tilt>
+      <div className={styles.container}>
+        <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image} />
+        <div className="info-section">
+          <h3 className={styles.title}>{title}</h3>
+          <p className={styles.description}>{description}</p>
+          {/* <ul className={styles.skills}>
         {
           skills.map((skill, id) => {
             return (<li key={id} className={styles.skill}>{skill}</li>)
 
           })}</ul> */}
-     </div>
-      <div className={styles.links}>
-        <a href={demo} className={styles.link}>Demo</a>
-        <a href={source} className={styles.link}>Source</a>
+        </div>
+        <div className={styles.links}>
+          <a href={demo} className={styles.link}>Demo</a>
+          <a href={source} className={styles.link}>Source</a>
+        </div>
       </div>
-    </div>)
+    </Tilt>
+  )
 }
