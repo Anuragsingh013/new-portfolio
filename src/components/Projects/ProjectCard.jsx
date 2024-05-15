c75e1bcf059c614260d5a3a6103336147e470ad8
anuragsingh013 <thakuranuragsingh099@gmail.com>
2024-05-15 18:06:05 +0530

    Added React parallax title functionality

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -6,24 +6,25 @@ import { getImageUrl } from "../../utils";
 export const ProjectCard = ({ project: { title, imageSrc, description, skills, demo, source } }) => {
 
   return (
-    <Tilt>
-      <div className={styles.container}>
-        <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image} />
-        <div className="info-section">
-          <h3 className={styles.title}>{title}</h3>
-          <p className={styles.description}>{description}</p>
-          {/* <ul className={styles.skills}>
+    // replace div with <Tilt> jb parralex effect hatana ho replace kr dena tilt ko div s (avoid css problem)
+    <Tilt className={styles.container}>
+
+      <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image} />
+      <div className="info-section">
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.description}>{description}</p>
+        {/* <ul className={styles.skills}>
         {
           skills.map((skill, id) => {
             return (<li key={id} className={styles.skill}>{skill}</li>)
 
           })}</ul> */}
-        </div>
-        <div className={styles.links}>
-          <a href={demo} className={styles.link}>Demo</a>
-          <a href={source} className={styles.link}>Source</a>
-        </div>
       </div>
+      <div className={styles.links}>
+        <a href={demo} className={styles.link}>Demo</a>
+        <a href={source} className={styles.link}>Source</a>
+      </div>
+
     </Tilt>
   )
 }
