f8d6509584621de2149cee86b1080748d1b26617
anuragsingh013 <thakuranuragsingh099@gmail.com>
2024-11-02 23:15:51 +0530

    updated resume

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <p className={styles.description}>I,m a front end developer with 2 years of
                 experince with React and Nodejs.
                 Reach out if you`d like to learn more!</p>
-                <a href={getImageUrl('Resume/LatestResume.pdf')} target='_blank' className={styles.contactBtn}>Download Resume</a>
+                <a href={getImageUrl('Resume/updatedResume.pdf')} target='_blank' className={styles.contactBtn}>Download Resume</a>
 
             </div>
             <img src={getImageUrl('hero/Anurag2.png')} alt="Hero img of me " className={styles.heroImg} />
