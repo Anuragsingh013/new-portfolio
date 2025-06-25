c28fb36c62d6c0da45408d31822b9ee82e51cf0c
anuragsingh013 <thakuranuragsingh099@gmail.com>
2025-06-26 00:41:39 +0530

    date work

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                     With a background in frontend web development using the MERN stack. 🤝 Let’s connect if you’d like to collaborate or know more!
                     <br />
                     <br />
-                    📅 Last updated : June 26, 2025               </p>
+                 Last updated : June 26, 2025               </p>
                 <a href={getImageUrl('Resume/updatedResume.pdf')} target='_blank' className={styles.contactBtn}>Download Resume</a>
 
             </div>
